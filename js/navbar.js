bd640567445a449eb6e55827fc2b8f4d821c5ba5
Joseph Szot <jsphszot@gmail.com>
2021-11-29 08:26:46 -0300

    first commit

---
+++ js/navbar.js
@@ -0,0 +1,74 @@
+var menu_button = document.getElementById("menu-button");
+var navbar = document.getElementById("navbar");
+var nav_ul = document.querySelector("nav ul");
+var menu_li = document.getElementsByClassName("menu-li");
+
+
+// Control navbar ----
+// navbar functions
+function ShowLoop(loopject, d_style=true) {
+    var displaystyle = (d_style) ? '' : 'block';
+   for (var i = 0; i < loopject.length; i++)
+       loopject[i].style.display = displaystyle;
+};
+function HideLoop(loopject) {
+    for (var i = 0; i < loopject.length; i++)
+       loopject[i].style.display = 'none';
+};
+function OpenM() {
+    menu_button.textContent = "🞬";
+    navbar.style.height = "100vh";
+    ShowLoop(menu_li, d_style=false);
+    // mainbod.style.display = 'none'
+};
+function CloseM(hide=true) {
+    menu_button.textContent = "☰";
+    navbar.style.height = "";
+    (hide === true) ? HideLoop(menu_li) : ShowLoop(menu_li);
+    // mainbod.style.display = ''
+};
+
+function OpenCloseMenu(){
+    if (menu_button.textContent == "☰"){
+        OpenM();
+    } else {
+        CloseM();
+    }
+};
+function CloseResized(){
+    // dummy function to close without hiding elements when window resize
+    CloseM(hide=false);
+};
+
+// - - - - - - - -
+// Fill navbar menu
+// - - - - - - - -
+function fillNavbar(){
+
+    var config_array;
+    fetch("../config/navbar.json")
+    .then(res => res.json())
+    .then(data => obj = data)
+    .then(() => fillNav(obj));
+    
+    
+    // https://medium.com/@akshaygarg576/how-to-use-templates-in-vanilla-javascript-d550e328b33b
+    // add nav elements
+    const fillNav = (config_array) => {
+        config_array.navbar.forEach((navlink) => {
+            nav_li = `
+            <li class='menu-li'>
+                <a href="${navlink.href}">${navlink.txt}</a>
+                </li>
+                `;
+                nav_ul.innerHTML += nav_li;
+            })
+        }
+        
+};
+
+
+// Runtime
+fillNavbar();
+menu_button.addEventListener('click', OpenCloseMenu);
+window.addEventListener('resize', CloseResized);
